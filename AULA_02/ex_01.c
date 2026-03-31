c19f058ea046b51c3cf719bfeaa22c93cf3c48a0
Guillherme Pires <guilhermemeanuel45@gmail.com>
2026-03-31 11:43:55 -0300

    aula 02 exercicios

---
+++ AULA_02/ex_01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+1) Classificação de Idade
+ Leia a idade de uma pessoa e informe:
+ Criança (0–12)
+ Adolescente (13–17)
+ Adulto (18–59)
+ Idoso (60+)
+*/
+
+
+int main() {
+
+ 
+ 
+
+}
